cd373d2989ee833c70cf2bbec173833a9ac8e141
tcya <tcya24@gmail.com>
2015-05-04 16:12:22 -0500

    Add the comparison of the time for training and prediction

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -30,8 +30,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 def NBAccuracy(features_train, labels_train, features_test, labels_test):
 	from sklearn.naive_bayes import GaussianNB
 	clf = GaussianNB()
+	t0 = time()
 	clf.fit(features_train, labels_train)
+	print "training time:", round(time()-t0, 3), "s"
+	t0 = time()
 	pred = clf.predict(features_test)
+	print "prediction time:", round(time()-t0, 3), "s"
 
 	from sklearn.metrics import accuracy_score
 	accuracy = accuracy_score(labels_test, pred)
