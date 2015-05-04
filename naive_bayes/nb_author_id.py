c932013b752e54ef19871a97929e28321d87f826
tcya <tcya24@gmail.com>
2015-05-04 15:46:26 -0500

    Add the naive Bayesian classifier

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -27,8 +27,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+	from sklearn.naive_bayes import GaussianNB
+	clf = GaussianNB()
+	clf.fit(features_train, labels_train)
+	pred = clf.predict(features_test)
 
-
+	from sklearn.metrics import accuracy_score
+	accuracy = accuracy_score(labels_test, pred)
+	return accuracy
 #########################################################
-
-
+accuracy = NBAccuracy(features_train, labels_train, features_test, labels_test)
+print accuracy
