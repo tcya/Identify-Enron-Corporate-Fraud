686344ff314f1f2479aecb93ecabed6e33e673fa
tcya <tcya24@gmail.com>
2015-05-05 03:28:53 -0500

    Add decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -22,12 +22,25 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
+# print len(features_train[0])
 
 #########################################################
 ### your code goes here ###
+def DTFit(features_train, labels_train, features_test):
+	# features_train = features_train[:len(features_train)/100] 
+	# labels_train = labels_train[:len(labels_train)/100] 
+	from sklearn import tree
+	clf = tree.DecisionTreeClassifier(min_samples_split=40)
+	# t0 = time()
+	clf.fit(features_train, labels_train)
+	# print "training time:", round(time()-t0, 3), "s"
+	# t0 = time()
+	pred = clf.predict(features_test)
+	# print "prediction time:", round(time()-t0, 3), "s"
+	return pred
 
-
+from sklearn.metrics import accuracy_score
+pred = DTFit(features_train, labels_train, features_test)
+accu = accuracy_score(labels_test, pred)
 #########################################################
-
-
+print accu
