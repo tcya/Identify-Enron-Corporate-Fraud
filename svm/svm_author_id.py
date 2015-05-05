6c1f05cc3fbeac68c0517208bbc7263b40b3d22b
tcya <tcya24@gmail.com>
2015-05-05 00:45:06 -0500

    Add SVMFit

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,7 +26,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+def SVMFit(features_train, labels_train, features_test):
+	from sklearn import svm
+	clf = svm.SVC(kernel='linear')
+	# t0 = time()
+	clf.fit(features_train, labels_train)
+	# print "training time:", round(time()-t0, 3), "s"
+	# t0 = time()
+	pred = clf.predict(features_test)
+	# print "prediction time:", round(time()-t0, 3), "s"
+	return pred
 
+from sklearn.metrics import accuracy_score
+pred = SVMFit(features_train, labels_train, features_test)
+accu = accuracy_score(labels_test, pred)
 #########################################################
+print accu
 
 
