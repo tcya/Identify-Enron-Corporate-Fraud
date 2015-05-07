8e35e47aa36fa748f590806eb15b49dfeaed6a6a
tcya <tcya24@gmail.com>
2015-05-07 07:49:19 -0500

    Primitive explore of emails

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,16 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-
+print len(enron_data)
+# print enron_data.keys()
+print len(enron_data['METTS MARK'])
+print [enron_data[i]['poi'] for i in enron_data].count(True)
+print enron_data['PRENTICE JAMES']
+print enron_data['COLWELL WESLEY']
+# print sorted(enron_data.keys())
+print enron_data['SKILLING JEFFREY K']
+print [enron_data[i]['total_payments'] for i in ['SKILLING JEFFREY K', 'LAY KENNETH L', 'FASTOW ANDREW S']]
+print len(enron_data) - [enron_data[i]['salary'] for i in enron_data].count('NaN')
+print len(enron_data) - [enron_data[i]['email_address'] for i in enron_data].count('NaN')
+print [enron_data[i]['total_payments'] for i in enron_data].count('NaN')
+print [enron_data[i]['total_payments'] for i in enron_data if enron_data[i]['poi'] == True].count('NaN')
