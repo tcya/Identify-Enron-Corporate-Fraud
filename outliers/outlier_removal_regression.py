4ca279fdc20d4615d420a12be8a5ff926ce3a86c
tcya <tcya24@gmail.com>
2015-05-08 05:35:20 -0500

    Add linear regression w/o cleaning

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -25,8 +25,14 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
-
+print reg.coef_
+print reg.intercept_
+print reg.score(ages_train, net_worths_train)
+print reg.score(ages_test, net_worths_test)
 
 
 
