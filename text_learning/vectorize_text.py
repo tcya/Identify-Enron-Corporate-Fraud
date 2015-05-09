0e51db2194a7bb6be0b664aa9a3cd5fa14fe66bf
tcya <tcya24@gmail.com>
2015-05-09 05:53:52 -0500

    Parse Sara and Chris' emails

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -42,19 +42,24 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
         if temp_counter < 200:
+        # if temp_counter:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            text = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
+            for word in ('sara', 'shackleton', 'chris', 'germani'):
+                text = text.replace(word, '')
             ### append the text to word_data
-
+            word_data.append(text)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == 'sara':
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
 
@@ -65,10 +70,9 @@ from_chris.close()
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
+print word_data[152]
 
 
 
 ### in Part 4, do TfIdf vectorization here
-
 
