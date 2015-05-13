b642ae1c0604a8ef918c787028e8e91fdeab4ee7
tcya <tcya24@gmail.com>
2015-05-12 23:13:52 -0500

    Find PCA varianve explained

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -76,6 +76,7 @@ print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.
 t0 = time()
 pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
 print "done in %0.3fs" % (time() - t0)
+print 'explained_variance: ', pca.explained_variance_ratio_
 
 eigenfaces = pca.components_.reshape((n_components, h, w))
 
