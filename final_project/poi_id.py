f0ab461c87437378af0408534f4fd78bf15d90cd
tcya <tcya24@gmail.com>
2015-05-18 10:36:14 -0500

    Clean up

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -9,27 +9,6 @@ import matplotlib.pyplot as plt
 from feature_format import featureFormat, targetFeatureSplit
 from tester import test_classifier, dump_classifier_and_data
 
-def Draw(data, features, feature_i, feature_j, remove_TOTAL = True, name="image.png", f1_name="feature 1", f2_name="feature 2"):
-    """ some plotting code designed to help you visualize your clusters """
-
-    ### plot each cluster with a different color--add more colors for
-    ### drawing more than 4 clusters
-    colors = ["b", "r", "k", "m", "g"]
-    if remove_TOTAL:
-        if data.has_key('TOTAL'):
-            del data['TOTAL']
-    f1_name = features[feature_i]
-    f2_name = features[feature_j]
-    poi = [data[i]['poi'] if (data[i]['poi'] != 'NaN' and data[i][f1_name] != 'NaN' and data[i][f2_name] != 'NaN') else 0 for i in data.keys()]
-    feature_1 = [data[i][features[feature_i]] if (data[i]['poi'] != 'NaN' and data[i][f1_name] != 'NaN' and data[i][f2_name] != 'NaN') else 0 for i in data.keys()]
-    feature_2 = [data[i][features[feature_j]] if (data[i]['poi'] != 'NaN' and data[i][f1_name] != 'NaN' and data[i][f2_name] != 'NaN') else 0 for i in data.keys()]
-
-    plt.scatter(feature_1, feature_2, color = [colors[1] if i else colors[0] for i in poi])
-    plt.xlabel(f1_name)
-    plt.ylabel(f2_name)
-    # plt.savefig(name)
-    plt.show()
-
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
@@ -38,33 +17,24 @@ features_financial = ['salary', 'deferral_payments', 'total_payments', 'loan_adv
 features_email = ['to_messages', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi','shared_receipt_with_poi', 'email_address']
 
 # features_list = ['poi','salary'] # You will need to use more features
-features_list = ['poi'] + features_financial + features_email[:-1]
-
+# features_list = ['poi'] + features_financial + features_email[:-1]
+features_list = ['poi', 'salary', 'bonus', 'deferred_income', 'total_stock_value', 'exercised_stock_options']
 ### Load the dictionary containing the dataset
 data_dict = pickle.load(open("final_project_dataset.pkl", "r") )
-for person in data_dict.keys():
-    data_dict[person]['fraction_from_poi'] = float(data_dict[person]['from_poi_to_this_person'])/float(data_dict[person]['to_messages'])
-    data_dict[person]['fraction_to_poi'] = float(data_dict[person]['from_this_person_to_poi'])/float(data_dict[person]['from_messages'])
+# for person in data_dict.keys():
+#     data_dict[person]['fraction_from_poi'] = float(data_dict[person]['from_poi_to_this_person'])/float(data_dict[person]['to_messages'])
+#     data_dict[person]['fraction_to_poi'] = float(data_dict[person]['from_this_person_to_poi'])/float(data_dict[person]['from_messages'])
+
 ### Task 2: Remove outliers
 del data_dict['TOTAL']
 
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
-# print my_dataset['CAUSEY RICHARD A']
-# Draw(my_dataset, features_list, 1, 1)
+
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
-from sklearn.feature_selection import SelectKBest
-# selection = SelectKBest(k=2)
-# features_new = selection.fit_transform(features, labels)
-# print np.where(selection.get_support()==True)[0]
-# print [features_list[1:][i] for i in np.where(selection.get_support()==True)[0]]
-# print map(features[0].tolist().index, features_new[0])
-# print features[2]
-# print features_new[2]
-# print my_dataset[sorted(my_dataset.keys())[2]]
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -72,6 +42,7 @@ from sklearn.feature_selection import SelectKBest
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
+# from sklearn.feature_selection import SelectKBest
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()    # Provided to give you a starting point. Try a varity of classifiers.
 
@@ -80,14 +51,18 @@ clf = GaussianNB()    # Provided to give you a starting point. Try a varity of c
 ### Because of the small size of the dataset, the script uses stratified
 ### shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
-for ii in range(1,len(features_list)):
-    selection = SelectKBest(k=ii)
-    features_new = selection.fit_transform(features, labels)
-    features_list_new = ['poi'] + [features_list[1:][i] for i in np.where(selection.get_support()==True)[0]]
-    print features_list_new
 
-    test_classifier(clf, my_dataset, features_list_new)
+##Finding the best features giving highest f1 score
+# for ii in range(1,len(features_list)):
+#     selection = SelectKBest(k=ii)
+#     features_new = selection.fit_transform(features, labels)
+#     features_list_new = ['poi'] + [features_list[1:][i] for i in np.where(selection.get_support()==True)[0]]
+#     print features_list_new
 
+#     test_classifier(clf, my_dataset, features_list_new)
+
+
+test_classifier(clf, my_dataset, features_list)
 ### Dump your classifier, dataset, and features_list so
 ### anyone can run/check your results.
 
